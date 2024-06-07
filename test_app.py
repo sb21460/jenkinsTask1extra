93bc105ea031bb7da7d627ad28b9e6f7da438b5a
Sandy <156678658+sb21460@users.noreply.github.com>
2024-06-07 10:59:20 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ class FlaskAppTests(unittest.TestCase):
     def test_home_page(self):
         response = requests.get(f"{self.BASE_URL}/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn("Hello QA", response.text)
+        self.assertIn("Hello Sandy", response.text)
         self.assertIn("I'm currently running in", response.text)
 
 
