a3b601d6cda2ec3e67efb0364287a563329d489e
chris-reeves1 <91483522+chris-reeves1@users.noreply.github.com>
2024-06-07 10:20:51 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,7 +7,9 @@ class FlaskAppTests(unittest.TestCase):
     def test_home_page(self):
         response = requests.get(f"{self.BASE_URL}/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn("failed", response.text)
+        self.assertIn("Hello QA", response.text)
+        self.assertIn("I'm currently running in", response.text)
+
 
 if __name__ == "__main__":
     unittest.main()
